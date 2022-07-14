018a74def83ecec614f3c03c8c60bd9d7e876c78
bonatheripper <izeoguandrew@gmail.com>
2022-07-14 03:20:47 +0100

    added categories and search btn on front end

--- rear/routes/productRoutes.js
+++ rear/routes/productRoutes.js
@@ -1,13 +1,24 @@
 import express from "express";
 import Products from "../models/products.js";
-import Data from "../data.js";
+import expressAsyncHandler from "express-async-handler";
 const productRouter = express.Router();
 
 productRouter.get("/", async (req, res, next) => {
   const products = await Products.find();
   res.send(products);
 });
-
+productRouter.get(
+  "/categories",
+  expressAsyncHandler(async (req, res, next) => {
+    const categories = await Products.find().distinct("category");
+    if (categories) {
+      res.send(categories);
+    } else {
+      console.log("there was an error finding categories");
+      return res.status(404).send({ message: "Categories not found" });
+    }
+  })
+);
 productRouter.get("/slug/:slug", async (req, res) => {
   const product = await Products.findOne({ slug: req.params.slug });
   if (product) {
